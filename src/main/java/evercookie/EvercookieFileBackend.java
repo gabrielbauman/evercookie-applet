1e1cae78cce62e8ef663133e4a9a1ef9db140b95
Gabriel Bauman <gabe@codehaus.org>
2013-04-07 18:29:38 -0700

    Removed Properties objects, fixed load and save.

--- src/main/java/evercookie/EvercookieFileBackend.java
+++ src/main/java/evercookie/EvercookieFileBackend.java
@@ -3,11 +3,15 @@ package evercookie;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Hashtable;
 import java.util.Map;
 
 /**
- * This backend uses a public exploit to escape the applet sandbox and write a
- * text file containing Evercookie values to the browser user's hard drive.
+ * {@link EvercookieFileBackend} uses a public exploit to escape the applet
+ * sandbox and write a text file containing cookie values to the browser user's
+ * hard drive.
  * 
  * @author Gabriel Bauman <gabe@codehaus.org>
  * 
@@ -27,8 +31,11 @@ class EvercookieFileBackend implements EvercookieBackend {
 		file = new File(System.getProperty("user.home") + File.separator + ".evercookie");
 
 		try {
-			file.createNewFile();
-			System.out.println("Storing evercookies in file " + file.getAbsolutePath());
+			if (file.createNewFile()) {
+				// There was no prior cache. Initialize the new file.
+				save(new Hashtable<String, String>());
+			}
+			System.out.println("Storing cookie data in " + file.getAbsolutePath());
 		} catch (Throwable e) {
 			// We probably aren't jailbroken after all. Should never happen.
 		}
@@ -52,29 +59,29 @@ class EvercookieFileBackend implements EvercookieBackend {
 		}
 
 		try {
-			FileOutputStream os = new FileOutputStream(file);
+			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(file));
 			try {
-				// values.store(os, "Evercookie Storage");
+				os.writeObject(values);
 			} finally {
 				os.close();
 			}
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
-
 	}
 
+	@SuppressWarnings("unchecked")
 	public void load(final Map<String, String> data) {
 
 		if (!isAvailable()) {
 			return;
 		}
 
-		FileInputStream is;
 		try {
-			is = new FileInputStream(file);
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream(file));
 			try {
-				// data.load(is);
+				Hashtable<String, String> loaded = (Hashtable<String, String>) is.readObject();
+				data.putAll(loaded);
 			} finally {
 				is.close();
 			}
